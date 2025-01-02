b225734fc34e7f62ef0332f8b35c84a09f5db7e5
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2025-01-02 22:00:11 +0700

    feat: add streambuilder for building up message

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -31,11 +31,12 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
-  void getMessages() async {
-    final messages = await _firestore.collection("messages").get();
-    for (var message in messages.docs) {
-      print(message.data());
-    }
+  void messagesStream() async {
+    _firestore.collection("messages").snapshots().listen((snapshot) {
+      for (var doc in snapshot.docs) {
+        print(doc.data());
+      }
+    });
   }
 
   @override
@@ -72,6 +73,33 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            StreamBuilder<QuerySnapshot>(
+              stream: _firestore.collection("messages").snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.lightBlueAccent,
+                    ),
+                  );
+                }
+
+                final messages = snapshot.data!.docs;
+                List<Text> messageWidgets = [];
+                for (var message in messages) {
+                  final messageText = message.get('text');
+                  final messageSender = message.get("sender");
+
+                  final messageWidget =
+                      Text('$messageText from $messageSender');
+                  messageWidgets.add(messageWidget);
+                }
+
+                return Column(
+                  children: messageWidgets,
+                );
+              },
+            ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
